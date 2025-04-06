f6fb1006b298416cab7f857a4bf1faa637113bae
LeeSeokWoon <leeseokwoon@naver.com>
2025-04-06 19:20:42 +0900

    feat:joinAll학습
    순서가 보장되어야 하는 JobBuilder 들을 joinAll에 Collection형태로 받아주는 joinAll

--- src/main/kotlin/coroutine/JobJoinTest.kt
+++ src/main/kotlin/coroutine/JobJoinTest.kt
@@ -1,11 +1,13 @@
 package coroutine
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main()= runBlocking {
+//    joinExam()
+    joinAllExam()
+}
+
+fun joinExam() = runBlocking {
     //Dispatcher.IO -> 해당 코루틴 JobBuilder가 I/O스레드임을 강조하기 위함
     val updateTokenJob = launch(Dispatchers.IO) {
         println("[${Thread.currentThread().name}] updateTokenJob start")
@@ -15,5 +17,24 @@ fun main()= runBlocking {
     updateTokenJob.join()
     val networkCallJob = launch(Dispatchers.IO) {
         println("[${Thread.currentThread().name}] networkCallJob 요청")
+    }
+}
+fun joinAllExam() = runBlocking {
+    val convertImgJob1 = launch(Dispatchers.IO) {
+        println("[${Thread.currentThread().name}] converImgJob start")
+        delay(100L)
+        println("[${Thread.currentThread().name}] converImgJob end")
+    }
+    val convertImgJob2 = launch(Dispatchers.IO) {
+        println("[${Thread.currentThread().name}] converImgJob start")
+        delay(100L)
+        println("[${Thread.currentThread().name}] converImgJob end")
+    }
+    /**
+     * public suspend fun joinAll(vararg jobs: Job): Unit = jobs.forEach { it.join() }
+     */
+    joinAll(convertImgJob1, convertImgJob2)
+    val updateImgJob = launch(Dispatchers.IO) {
+        println("upload image complete")
     }
 }
