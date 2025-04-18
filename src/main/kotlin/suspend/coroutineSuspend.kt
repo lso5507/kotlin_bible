d28d7cf51b48664d1b65068f89b89149729feec9
lso5507 <49707719+lso5507@users.noreply.github.com>
2025-04-18 16:44:55 +0900

    CoroutineScope_Suspend 학습

---
+++ src/main/kotlin/suspend/coroutineSuspend.kt
@@ -0,0 +1,33 @@
+package suspend
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import utils.getElapsedTime
+
+fun main()= runBlocking{
+    val startTime = System.currentTimeMillis()
+    val result = searchByKeyword("Star")
+    println("result::${result.toList()}")
+    println("end::${getElapsedTime(startTime)}")
+}
+suspend fun searchByKeyword(keyword:String):Array<String> = coroutineScope{
+    val dbResultsDeferred = async{
+        searchFormDB(keyword)
+    }
+    val serverResultsDeferred = async{
+        searchFormServer(keyword)
+    }
+    //coroutineScope로 return 되게끔 지정
+    // * 는 Collections의 데이터를 스프레드 연산자로 펼쳐낸 것
+    return@coroutineScope arrayOf(*dbResultsDeferred.await(),*serverResultsDeferred.await())
+}
+suspend fun searchFormDB(keyword:String):Array<String>{
+    delay(1000L);
+    return arrayOf("db ${keyword}","db ${keyword}")
+}
+suspend fun searchFormServer(keyword:String):Array<String>{
+    delay(1000L);
+    return arrayOf("db ${keyword}","db ${keyword}")
+}
