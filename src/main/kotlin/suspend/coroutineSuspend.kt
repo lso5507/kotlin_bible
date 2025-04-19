13ecdbd8eb737239a950671c2cd6e05312c3c033
lso5507 <49707719+lso5507@users.noreply.github.com>
2025-04-19 18:36:06 +0900

    suspend_async try catch 학습

--- src/main/kotlin/suspend/coroutineSuspend.kt
+++ src/main/kotlin/suspend/coroutineSuspend.kt
@@ -19,9 +19,19 @@ suspend fun searchByKeyword(keyword:String):Array<String> = coroutineScope{
     val serverResultsDeferred = async{
         searchFormServer(keyword)
     }
+    val dbResult =try{
+        dbResultsDeferred.await()
+    }catch(ex:Exception){
+        arrayOf()
+    }
+    val serverResult =try{
+        serverResultsDeferred.await()
+    }catch(ex:Exception){
+        arrayOf()
+    }
     //coroutineScope로 return 되게끔 지정
     // * 는 Collections의 데이터를 스프레드 연산자로 펼쳐낸 것
-    return@coroutineScope arrayOf(*dbResultsDeferred.await(),*serverResultsDeferred.await())
+    return@coroutineScope arrayOf(*dbResult,*serverResult)
 }
 suspend fun searchFormDB(keyword:String):Array<String>{
     delay(1000L);
