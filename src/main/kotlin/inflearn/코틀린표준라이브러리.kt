01b405e322557c7199b050757cefa908d9b85604
LeeSeokWoon <leeseokwoon@naver.com>
2025-08-31 20:23:45 +0900

    Update 코틀린표준라이브러리.kt

--- src/main/kotlin/inflearn/코틀린표준라이브러리.kt
+++ src/main/kotlin/inflearn/코틀린표준라이브러리.kt
@@ -14,3 +14,9 @@ fun todoCall(){
     TODO("코틀린 에서는 TODO 주석대신 이렇게 처리가능 해당 TODO 함수가 호출되면 NotImplementedError 발생")
 
 }
+fun require(num:Int){
+    require(num>0){"0보다 커야합니다."}
+}
+fun check(num:Int){
+    check(num>0){"0보다 커야합니다."}
+}
